3b34cb85eca407be2d425336c874149cc909fd09
Priyanka123Chauhan <priyankachauhan.career@gmail.com>
2025-06-22 22:07:39 +0530

    Update [id].js

--- pages/table/[id].js
+++ pages/table/[id].js
@@ -32,6 +32,8 @@ export default function Table() {
         const allowedPrefixes = ['2402:e280', '58.84'];
         const allowed = allowedPrefixes.some(prefix => ip?.startsWith(prefix));
         setIsAllowed(allowed);
+        window.location.reload(); 
+
       } catch (err) {
         console.error('IP check failed:', err);
       } finally {
